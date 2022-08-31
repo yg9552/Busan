4ca58afe0bb30eeed5321cd86347cfb6291c747b
yg9552 <yg458@naver.com>
2022-08-31 15:54:32 +0900

    .

--- src/main/java/com/yg/infra/modules/codegroup/CodeGroup.java
+++ src/main/java/com/yg/infra/modules/codegroup/CodeGroup.java
@@ -2,4 +2,31 @@ package com.yg.infra.modules.codegroup;
 
 public class CodeGroup {
 
+//	codegroup
+	private String seq;
+	private String name;
+	private Integer DelNy;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getDelNy() {
+		return DelNy;
+	}
+	public void setDelNy(Integer delNy) {
+		DelNy = delNy;
+	}
+	
+	
+	
+	
 }
