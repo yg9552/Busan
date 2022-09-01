0f6ae8f7420630e2f2715c9ccd25d8c56414cacd
yg9552 <yg458@naver.com>
2022-09-01 16:02:29 +0900

    .

--- src/main/java/com/yg/infra/modules/code/Code.java
+++ src/main/java/com/yg/infra/modules/code/Code.java
@@ -1,30 +1,42 @@
 package com.yg.infra.modules.code;
 
+import com.yg.infra.modules.codegroup.CodeGroup;
+
 public class Code {
 
+	CodeGroup codeGroup;
+	
+	public CodeGroup getCodeGroup() {
+		return codeGroup;
+	}
+	public void setCodeGroup(CodeGroup codeGroup) {
+		this.codeGroup = codeGroup;
+	}
 	
 	private String seq;
 	private String name;
 	private String cg_seq;
-	
-	
-	public String getSeq() {
-		return seq;
-	}
-	public void setSeq(String seq) {
-		this.seq = seq;
-	}
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	
 	public String getCg_seq() {
 		return cg_seq;
 	}
 	public void setCg_seq(String cg_seq) {
 		this.cg_seq = cg_seq;
 	}
+	
+	
 	
 }
