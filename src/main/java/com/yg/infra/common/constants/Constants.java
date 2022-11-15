5a62ecc39d55fe8ea59e049738ef2c960e34ce12
ezen <ezen@DESKTOP-O03MT84>
2022-11-15 09:17:52 +0900

    .

--- src/main/java/com/yg/infra/common/constants/Constants.java
+++ src/main/java/com/yg/infra/common/constants/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
 	public static final long PASSWOPRD_CHANGE_INTERVAL = 90;
 	
 	public static final String UPLOAD_PATH_PREFIX = "D://factory/ws_sts_4151/busan/src/main/webapp/resources/uploaded";
+//	public static final String UPLOAD_PATH_PREFIX = "/factory/workspace/Busan/resources/uploaded";
 	public static final String UPLOAD_PATH_PREFIX_FOR_VIEW = "/resources/uploaded";
 	
 	public static final int COOKIE_MAXAGE = 60 * 60 * 24 * 30;	//초 * 분 * 시간 * 일 : 30day
